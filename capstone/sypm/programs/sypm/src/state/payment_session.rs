df03d9a34d38237cb8b6dae869b0bec9cb58fafd
Ayush Soni <ayushsoni1021@gmail.com>
2025-08-15 02:11:05 +0530

    update payment_session

--- capstone/sypm/programs/sypm/src/state/payment_session.rs
+++ capstone/sypm/programs/sypm/src/state/payment_session.rs
@@ -4,11 +4,19 @@ use anchor_lang::prelude::*;
 pub struct PaymentSession {
     pub user: Pubkey,  // 32
     pub merchant: Pubkey, // 32
+    pub preferred_token: Pubkey, // 32
     pub split_tokens: Vec<(Pubkey,u64)>, // 4 + ((32+8) × max_len) bytes
+    pub total_requested: u64, // 8 - Amount merchant should receive in preferred token
     pub status: u8,  // 1
     pub bump: u8,   // 1
 }
 
 impl Space for PaymentSession {
-   const INIT_SPACE: usize = 32 + 32 + 4 + ((32 + 8) * 5) + 1 + 1;
+   const INIT_SPACE: usize = 32 + 32 + 32 + 4 + ((32 + 8) * 5) + 8 + 1 + 1;
+}
+
+pub enum PaymentStatus {
+    Pending = 0,
+    Completed = 1,
+    Cancelled = 2,
 }
